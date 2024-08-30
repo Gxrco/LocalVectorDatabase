0686c3f46889932dc910409b3db4bf4041358b14
Gerson Ramirez <ram22281@uvg.edu.gt>
2024-08-30 15:32:38 -0600

    feat: create the agent to process the file

--- main.py
+++ main.py
@@ -10,3 +10,41 @@ from langchain import hub
 
 load_dotenv()
 
+if __name__ == "__main__":
+    print("Carga del archivo PDF...")
+    pdf_path = os.getenv("FILE_PATH_PDF")
+    loader = PyPDFLoader(file_path=pdf_path)
+    documents = loader.load()
+
+    print("Dividir el PDF en chunks")
+    text_splitter = CharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=30, separator="\n"
+    )
+
+    docs = text_splitter.split_documents(documents=documents)
+
+    print("Guardar los chunks en una base vectorial local")
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_documents(docs, embeddings)
+    vector_store.save_local("faiss_index_react")
+
+    print("Carga de embeddings de la base vectorial local")
+    new_vector_store = FAISS.load_local(
+        folder_path="faiss_index_react", embeddings=embeddings, allow_dangerous_deserialization=True,
+    )
+
+    print("Creación y ejecución del agente")
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(
+        OpenAI(), retrieval_qa_chat_prompt
+    )
+
+    retrieval_chain = create_retrieval_chain(
+        new_vector_store.as_retriever(), combine_docs_chain
+    )
+
+    res = retrieval_chain.invoke({"input": "Give me the gist of ReAct in 3 sentences"})
+
+    print(res["answer"])
+
+    print("Final del proceso")
